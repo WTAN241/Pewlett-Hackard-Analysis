17d4625c9c182325e83a0ec8bff81d875d72d714
Wenyi Tan <wenyi.tan.1@gmail.com>
2021-03-24 12:39:27 -0400

    Updated SQL tables

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 -- Creating tables for PH-EmployeeDB
-CREATE TABLE departments (dept_no VARCHAR(4) NOT NULL,
+CREATE TABLE departments (dept_no VARCHAR NOT NULL,
 						 dept_name VARCHAR(40) NOT NULL,
 						 PRIMARY KEY(dept_no),
 						 UNIQUE(dept_name));
@@ -12,7 +12,7 @@ CREATE TABLE employees (emp_no INT NOT NULL,
 					   hire_date DATE NOT NULL,
 					   PRIMARY KEY(emp_no));
 					  
-CREATE TABLE dept_manager (dept_no VARCHAR(4) NOT NULL,
+CREATE TABLE dept_manager (dept_no VARCHAR NOT NULL,
 						  emp_no INT NOT NULL,
 						  from_date DATE NOT NULL,
 						  to_date DATE NOT NULL,
@@ -27,11 +27,10 @@ CREATE TABLE salaries (emp_no INT NOT NULL,
 					  PRIMARY KEY(emp_no),
 					  FOREIGN KEY(emp_no) REFERENCES employees (emp_no));	
 					 
-CREATE TABLE dept_emp (dept_no VARCHAR(4) NOT NULL,
-					  emp_no INT NOT NULL,
+CREATE TABLE dept_emp (emp_no INT NOT NULL,
+					  dept_no VARCHAR NOT NULL
 					  from_date DATE NOT NULL,
 					  to_date DATE NOT NULL,
-					  PRIMARY KEY(emp_no),
 					  FOREIGN KEY(dept_no) REFERENCES departments (dept_no),
 					  FOREIGN KEY(emp_no) REFERENCES employees (emp_no),
 					  FOREIGN KEY(emp_no) REFERENCES salaries (emp_no));	
@@ -40,7 +39,6 @@ CREATE TABLE titles (emp_no INT NOT NULL,
 					title VARCHAR NOT NULL,
 					from_date DATE NOT NULL,
 					to_date DATE NOT NULL,
-					PRIMARY KEY(emp_no),
 					FOREIGN KEY(emp_no) REFERENCES salaries (emp_no),
 					FOREIGN KEY(emp_no) REFERENCES employees (emp_no));		
 					
